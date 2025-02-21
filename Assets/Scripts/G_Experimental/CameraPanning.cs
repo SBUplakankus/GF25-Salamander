3d78c20ec143b73f65e78a23fba8a718377817ba
Gabrielius <D00265365@student.dkit.ie>
2025-02-21 20:48:59 +0000

    Tweaking with camera in end scene

--- Assets/Scripts/G_Experimental/CameraPanning.cs
+++ Assets/Scripts/G_Experimental/CameraPanning.cs
@@ -7,13 +7,21 @@ public class CameraPanning : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke("PanCamera", 5f);
+        Invoke("TestReset", 5f);
     }
 
     void PanCamera()
     {
-        Tween.PositionZ(camera.transform, -12, 10f);
-        Tween.Custom(60, 15, duration: 10f, onValueChange: newVal => Camera.main.fieldOfView = newVal);
+        Tween.PositionZ(camera.transform, 2, 15f);
+        Tween.Custom(30, 60, duration: 15f, onValueChange: newVal  => Camera.main.fieldOfView = newVal);
+        Tween.Delay(duration: 16f, () => Tween.Custom(60, 20, duration: 4f, onValueChange: newVal => Camera.main.fieldOfView = newVal));
+        Invoke("TestReset", 25f);
     }
-  
+
+    void TestReset()
+    {
+        Camera.main.fieldOfView = 30;
+        camera.transform.position = new Vector3(0f, 1.7f, 7f);
+        Invoke("PanCamera", 10f);
+    }
 }
