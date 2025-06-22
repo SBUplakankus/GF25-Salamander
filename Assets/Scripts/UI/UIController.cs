41681121cfd2b02d30010c4fbe9dc9a771d5b663
kochilLu <72820763+kochilLu@users.noreply.github.com>
2025-06-22 14:15:57 +0100

    in a copy of the main game improved the bad consumable objects
    -> changed their texture to a slightly smuddgier one
    -> chanaged the color of the particles
    -> slightly changed how the bottles were placed

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -93,7 +93,7 @@ namespace UI
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Space))
             {
                 HandlePauseMenuDisplay();
             }
